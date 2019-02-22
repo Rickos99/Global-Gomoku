9a94b56d5e5164a8af217e1df884fa2aba512fb1
Rickard Bemm <rickos99@hotmail.com>
2019-02-22 14:29:46 +0100

    Update GomokuGUI.java

--- src/lab4/gui/GomokuGUI.java
+++ src/lab4/gui/GomokuGUI.java
@@ -1,9 +1,17 @@
 package lab4.gui;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import lab4.client.GomokuClient;
-import lab4.data.GameGrid;
 import lab4.data.GomokuGameState;
 
 /*
@@ -11,9 +19,28 @@ import lab4.data.GomokuGameState;
  */
 
 public class GomokuGUI implements Observer{
+	
+	public static void main(String[] args) {
+		GomokuClient gc = new GomokuClient(4000);
+		GomokuGameState gs = new GomokuGameState(gc);
+		new GomokuGUI(gs, gc).createLayout();
+	}
 
 	private GomokuClient client;
 	private GomokuGameState gamestate;
+	
+	// Buttons
+	private JButton connectButton;
+	private JButton newGameButton;
+	private JButton disconnectButton;
+	
+	// Labels
+	private JLabel messageLabel;
+	
+	// Panels
+	private JPanel gameGridPanel;
+	private JPanel buttonPanel;
+	private JPanel messagePanel;
 	
 	/**
 	 * The constructor
@@ -26,8 +53,6 @@ public class GomokuGUI implements Observer{
 		this.gamestate = g;
 		client.addObserver(this);
 		gamestate.addObserver(this);
-		
-		
 	}
 	
 	
@@ -53,4 +78,46 @@ public class GomokuGUI implements Observer{
 		
 	}
 	
+	private void createLayout() {
+		gameGridPanel = new GamePanel(gamestate.getGameGrid());
+		buttonPanel = new JPanel();
+		messagePanel = new JPanel();
+		
+		buttonPanel.add(new JButton("Connect"));
+		
+		Dimension dimension = new Dimension(200, 400);
+		gameGridPanel.setSize(dimension);
+		buttonPanel.setSize(dimension);
+		messagePanel.setSize(dimension);
+		
+		gameGridPanel.setBackground(Color.RED);
+		buttonPanel.setBackground(Color.ORANGE);
+		messagePanel.setBackground(Color.GREEN);
+		
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		layout.setConstraints(gameGridPanel, c);
+		
+		c.gridx = 0;
+		c.gridy = 1;
+		layout.setConstraints(buttonPanel, c);
+		
+		c.gridx = 0;
+		c.gridy = 2;
+		layout.setConstraints(messagePanel, c);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(layout);
+		panel.add(gameGridPanel);
+		panel.add(buttonPanel);
+		panel.add(messagePanel);
+		
+		JFrame frame = new JFrame("GUI");
+		frame.setContentPane(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
